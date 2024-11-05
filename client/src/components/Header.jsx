3ccf6717ea7a3032ee8680bbaea79164b509fcbb
Fola <raimiolajuwon@gmail.com>
2024-11-05 03:59:49 -0800

    create and run the server

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -10,7 +10,7 @@ export default function Header() {
            <span className='text-slate-500'>Fola</span>
            <span className='text-slate-700'>Estate</span>
          </h1>
-         </Link>
+        </Link>
          <form className='bg-slate-100 p-3 rounded-lg flex items-center'>
            <input type="text" placeholder="Search..." className='bg-transparent focus:outline-none w-24 sm:w-64' />
            <FaSearch className='text-slate-600'/>
@@ -23,7 +23,7 @@ export default function Header() {
             <Link to='/about'>
               <li className='hidden sm:inline text-slate-700 hover:underline'>About</li>
             </Link>
-            
+
             <Link to='/sign-in'>
               <li className='text-slate-700 hover:underline'>Sign in</li>
             </Link>
